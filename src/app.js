0e78b585fdbbd3858ea8a22f5ff6c63e0d81b4c2
IsabellaSoares <isabella.soares.28@gmail.com>
2020-10-29 21:30:27 -0300

    create post route

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 
-// const { v4: uuid, validate: isUuid } = require('uuid');
+const { v4: uuid, validate: isUuid } = require('uuid');
 
 const app = express();
 
@@ -10,12 +10,28 @@ app.use(cors());
 
 const repositories = [];
 
+const validateProjectId = (request, response, next) => {
+
+  const { id } = request.params
+
+  if (!isUuid(id)) {
+    return response.status(400).json({ error: 'Invalid project ID.' })
+  }
+
+  return next()
+}
+
 app.get("/repositories", (request, response) => {
   // TODO
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+  const { title, url, techs } = request.body
+  const repository = { id: uuid(), title, url, techs, likes: 0 }
+
+  repositories.push(repository)
+
+  return response.json(repository)
 });
 
 app.put("/repositories/:id", (request, response) => {

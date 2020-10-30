8e941a413070c9c067d7aa1eafbf3df65f737e0e
IsabellaSoares <isabella.soares.28@gmail.com>
2020-10-29 22:30:25 -0300

    fix: disable manually likes update

--- src/app.js
+++ src/app.js
@@ -45,7 +45,7 @@ app.put("/repositories/:id", (request, response) => {
 
   const { title, url, techs } = request.body
 
-  const repository = { id, title, url, techs }
+  const repository = { id, title, url, techs, likes: repositories[repositoryIndex].likes }
 
   repositories[repositoryIndex] = repository
 

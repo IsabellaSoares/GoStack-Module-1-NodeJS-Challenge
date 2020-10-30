1bbc8198ccdc6af35caadd6bd79731cbd334ae5c
IsabellaSoares <isabella.soares.28@gmail.com>
2020-10-29 21:39:21 -0300

    create put route

--- src/app.js
+++ src/app.js
@@ -24,7 +24,20 @@ app.post("/repositories", (request, response) => {
 });
 
 app.put("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params
+  const repositoryIndex = repositories.findIndex(repository => repository.id === id)
+
+  if (repositoryIndex < 0) {
+    return response.status(400).json({ error: 'Repository not found.' })
+  }
+
+  const { title, url, techs } = request.body
+
+  const repository = { id, title, url, techs }
+
+  repositories[repositoryIndex] = repository
+
+  return response.json(repository)
 });
 
 app.delete("/repositories/:id", (request, response) => {
